93e0c30bd6b4b881b7c0197f59093bc3bba225e9
Marcel Petrick <mail@marcelpetrick.it>
2020-08-06 18:23:22 +0200

    3d Gauss file

---
+++ 3D_gauss.py
@@ -0,0 +1,26 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+X = np.arange(-2, 2, 0.1)
+Y = np.arange(-2, 2, 0.1)
+X, Y = np.meshgrid(X, Y)
+R = X**2 + Y**2
+Z = np.exp(-R)
+
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+ax.set_zlim(-1.01, 1.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
