8fc7525a8ad0c5bbc534e8c583c68bb1bbf94f68
Marcel Petrick <mail@marcelpetrick.it>
2020-08-06 18:19:23 +0200

    corkscrew plot taken from tu matieral and updated with some wacky steps (33 instead of 100)

---
+++ 3D_corkscrew.py
@@ -0,0 +1,18 @@
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+
+mpl.rcParams['legend.fontsize'] = 10
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+theta = np.linspace(-4 * np.pi, 4 * np.pi, 33) # linear zahlenraum von -4pi to +4 pi with 100 steps
+z = np.linspace(-2, 2, 33)
+r = + 1
+x = r * np.sin(theta)
+y = r * np.cos(theta)
+ax.plot(x, y, z, label='parametric curve')
+ax.legend()
+
+plt.show()
