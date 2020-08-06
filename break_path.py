20288dbc01ec687d807d8a87542f2e92373d6b22
Marcel Petrick <mail@marcelpetrick.it>
2020-08-06 18:52:12 +0200

    exercise 1: better and cleaner

--- break_path.py
+++ break_path.py
@@ -3,7 +3,6 @@ speed = [25, 50, 100, 150, 200] # km/h
 reactionDistance = [8, 15, 30, 45, 60] # m
 breakDistance = [7, 25, 100, 225, 400] # m
 stoppingDistance = [a + b for a,b in zip(reactionDistance, breakDistance)] # too lazy to write the prepared sums
-print(stoppingDistance)
 
 #-----------------------------------------------------------------
 
@@ -12,13 +11,14 @@ def makeAPlot():
 
     plt.plot(speed, reactionDistance, label = "reaction distance")
     plt.plot(speed, breakDistance, label = "breaking distance")
-    plt.plot(speed, stoppingDistance, label = "stoppping distance")
-    plt.legend()
+    plt.plot(speed, stoppingDistance, label = "stopping distance")
+    plt.legend() # add a legend for the graphs (with colors)
+
+    plt.title("breaking distance plotted against the speed")
+    plt.grid(color = 'grey', linestyle = '-.')
 
     plt.xlabel("speed in km/h")
     plt.ylabel("distance in m")
-
-    # todo add a legend for the graphs (with colors)
 
     plt.show()
 
