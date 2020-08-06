54416a1b56026b9ebee70c61177edbd6f2355d97
Marcel Petrick <mail@marcelpetrick.it>
2020-08-06 20:12:45 +0200

    pygame: added more clock examples

---
+++ analog_clock_experimental_02.py
@@ -0,0 +1,45 @@
+import pygame, time
+from pygame.locals import *
+from math import sin, cos, radians
+
+pygame.init()
+
+RED = (255, 0, 0)
+WHITE= (255, 255, 255)
+BLACK = (0, 0, 0)
+
+dialFIELD = pygame.display.set_mode((400, 400))
+dialFIELD.fill(WHITE)
+
+MX = 200
+MY = 200
+MP = ((MX, MY))
+
+def dot(radius, angle):
+    # draw a dot @ circumference wit radius and angle
+    w1 = radians(angle * 6 - 90)
+    x1 = int(MX + radius * cos(w1))
+    y1 = int(MY + radius * sin(w1))
+    return((x1, y1))
+
+
+pygame.draw.circle(dialFIELD, BLACK, dot(190, 2), 2)
+
+for i in range(60):
+    pygame.draw.circle(dialFIELD, BLACK, dot(190, i), 2)
+
+
+for i in range(12):
+    pygame.draw.circle(dialFIELD, BLACK, dot(190, i * 5), 4)
+
+
+mainloop = True
+
+while mainloop:
+    pygame.display.update()
+    for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+                mainloop = False
+
+pygame.quit()
+
