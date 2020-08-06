4a82e026a3c1160383e905c681e5ec14d6d77b9b
Marcel Petrick <mail@marcelpetrick.it>
2020-08-06 18:49:04 +0200

    exercise: final version with some screenshot

--- break_path.py
+++ break_path.py
@@ -1,24 +1,19 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-from mpl_toolkits.mplot3d import Axes3D
-
 # the data
 speed = [25, 50, 100, 150, 200] # km/h
 reactionDistance = [8, 15, 30, 45, 60] # m
 breakDistance = [7, 25, 100, 225, 400] # m
-#stoppingDistance = reactionDistance + breakDistance # too lazy to write
-#print(stoppingDistance)
+stoppingDistance = [a + b for a,b in zip(reactionDistance, breakDistance)] # too lazy to write the prepared sums
+print(stoppingDistance)
 
 #-----------------------------------------------------------------
 
 def makeAPlot():
     import matplotlib.pyplot as plt
 
-
-    plt.plot(speed, reactionDistance)
-    plt.plot(speed, breakDistance)
-    #plt.plot(speed, stoppingDistance)
+    plt.plot(speed, reactionDistance, label = "reaction distance")
+    plt.plot(speed, breakDistance, label = "breaking distance")
+    plt.plot(speed, stoppingDistance, label = "stoppping distance")
+    plt.legend()
 
     plt.xlabel("speed in km/h")
     plt.ylabel("distance in m")
